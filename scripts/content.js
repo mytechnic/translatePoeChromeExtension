b11271c5bdcec07579449d60115038bd5f977cb7
mytechnic <mytechnic@gmail.com>
2024-05-03 02:09:01 +0900

    POE 스타 한글 번역 1.0

--- scripts/content.js
+++ scripts/content.js
@@ -1,51 +1,219 @@
-db = {};
-keys = {};
+let version = {};
+let dictionary = {};
+let isInProgressTranslating = false;
+let autoTranslate = false;
+let documentTextCache = {};
+let allInitialized = false;
+let cacheInitialized = false;
+let syncInitialized = false;
+let url = window.location.href;
 
-function entries(from, to) {
-    let db = {};
-    for (let i in from) {
-        for (let key in from[i]) {
-            if (key === 'disc' || key === 'flags') {
-                continue;
-            }
-            db[from[i][key]] = to[i][key];
-            keys[key] = key;
-        }
+function getPageUrl(url) {
+    let cleanUrl = url.split('?')[0];
+
+    if (cleanUrl.endsWith('/')) {
+        cleanUrl = cleanUrl.slice(0, -1);
     }
-    return db;
+
+    return cleanUrl;
 }
 
-function label(from, to) {
-    let db = {};
-    db[from['label']] = to['label'];
-    return db;
+function getPathUrl(url) {
+    const urlObject = new URL(url);
+
+    const pathParts = urlObject.pathname.split('/').filter(part => part !== '');
+    const parentPath = pathParts.slice(0, -1).join('/') + '/';
+
+    if (url.endsWith('/')) {
+        return url;
+    }
+
+    return parentPath;
 }
 
-function extractText(node) {
-    if (node.nodeType === 3) {
-        if (node.nodeValue && db[node.nodeValue]) {
-            node.nodeValue = db[node.nodeValue];
-        }
-    }
-    node.childNodes.forEach(child => {
-        extractText(child);
+function cacheStorage() {
+    let c = new Promise(function (resolve, reject) {
+        chrome.storage.local.get().then((data) => {
+            version = data['version'];
+            dictionary = data['dictionary'];
+            cacheInitialized = true;
+            allInitialized = cacheInitialized && syncInitialized;
+        });
     });
 }
 
-function loadTranslate() {
-    for (let i in data['us'].items.result) {
-        let from = data['us'].items.result[i];
-        let to = data['kr'].items.result[i];
-        Object.assign(db, label(from, to));
-        Object.assign(db, entries(from.entries, to.entries));
+async function syncStorage() {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+            if (namespace === 'local' && key === 'auto') {
+                autoTranslate = newValue === '1';
+                console.log('autoTranslate', autoTranslate);
+            }
+        }
+    });
+
+    let c = new Promise(function (resolve, reject) {
+        chrome.storage.sync.get().then((storage) => {
+            const page = getPageUrl(url);
+            const path = getPathUrl(url);
+
+            if (!storage['page']) {
+                storage['page'] = {};
+            }
+
+            if (!storage['path']) {
+                storage['path'] = {};
+            }
+
+            autoTranslate = !!(storage['page'][page] || storage['path'][path]);
+            syncInitialized = true;
+            allInitialized = cacheInitialized && syncInitialized;
+        });
+    });
+}
+
+function toLowerCase(text) {
+    if (text == null) {
+        return text;
+    }
+    return text.toLowerCase();
+}
+
+function translateText(source) {
+    let word = source.trim();
+    let hashKey = toLowerCase(word);
+    let findText = '';
+
+    if (dictionary['h'][hashKey]) {
+        findText = source.replaceAll(word, dictionary['h'][hashKey]);
+    } else if (dictionary['p'][hashKey]) {
+        findText = source.replaceAll(word, dictionary['p'][hashKey]);
+    }
+
+    if (!findText) {
+        for (let key in dictionary['p']) {
+            findText = regexTranslate(source, key, dictionary['p'][key]);
+            if (findText) {
+                break;
+            }
+        }
+    }
+
+    if (findText) {
+        return findText;
+    }
+
+    return null;
+}
+
+function regexTranslate(text, source, target) {
+    const pattern = source.replace(/\+/g, '\\+').replace(/#/g, '(\\d+)');
+    const regex = new RegExp(pattern, 'i');
+    const matched = text.trim().match(regex);
+    if (matched) {
+        for (let i = 1; i < matched.length; i++) {
+            target = target.replace(/#/, matched[i]);
+        }
+        text = target.replace(regex, target);
+        return text;
     }
 }
 
-function translater() {
-    loadTranslate();
-    extractText(document.body);
+function translate(node) {
+    if (node.nodeType === 3) {
+        if (!node.nodeValue) {
+            return;
+        }
+
+        if (documentTextCache[node.nodeValue] === 1) {
+            return;
+        } else if (documentTextCache[node.nodeValue]) {
+            node.nodeValue = documentTextCache[node.nodeValue];
+            return;
+        }
+
+        let text = translateText(node.nodeValue);
+        if (text) {
+            documentTextCache[node.nodeValue] = text;
+            node.nodeValue = text;
+        } else {
+            documentTextCache[node.nodeValue] = 1;
+        }
+    }
+
+    node.childNodes.forEach(child => {
+        translate(child);
+    });
+}
+
+function translateTask(sDebug) {
+
+    console.log('allInitialized', allInitialized);
+    console.log('isInProgressTranslating', isInProgressTranslating);
+    console.log('autoTranslate', autoTranslate);
+
+    if (!allInitialized) {
+        return;
+    }
+
+    if (isInProgressTranslating) {
+        return;
+    }
+
+    if (!autoTranslate) {
+        return;
+    }
+
+    isInProgressTranslating = true;
+    console.log('translateTask')
+    translate(document.body);
+    isInProgressTranslating = false;
+}
+
+async function loadDictionary() {
+    await syncStorage();
+    await cacheStorage();
+
+    let _version;
+    if (dictionary) {
+        $.ajax({
+            async: false,
+            dataType: 'json',
+            url: 'https://mytechnic.github.io/translate/poe_kr_version.json',
+            success: function (result) {
+                _version = result['version'];
+                if (_version > version) {
+                    version = -1;
+                }
+            }
+        });
+
+        if (version > -1) {
+            return;
+        }
+    }
+
+    $.ajax({
+        async: false,
+        dataType: 'json',
+        url: 'https://mytechnic.github.io/translate/poe_kr.json',
+        success: function (result) {
+            dictionary = result;
+            chrome.storage.local.set({'version': _version, 'dictionary': dictionary});
+        }
+    });
+}
+
+function translateExecutor() {
+    isInProgressTranslating = true;
+    loadDictionary();
+    isInProgressTranslating = false;
+
+    setInterval(function () {
+        translateTask();
+    }, 300);
 }
 
 $(function () {
-    translater();
+    translateExecutor();
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,50 +1,165 @@
-$(function () {
-    let init = function () {
-        chrome.storage.sync.get().then((data) => {
-            console.log(data);
-            let language = data['language'] || 'ko';
-            let auto_check = data['auto'] || '1';
-            setLanguage(language);
-            setAuto(auto_check === '1');
+let url = '';
+let storage = {};
+
+function getPageUrl(url) {
+    let cleanUrl = url.split('?')[0];
+
+    if (cleanUrl.endsWith('/')) {
+        cleanUrl = cleanUrl.slice(0, -1);
+    }
+
+    return cleanUrl;
+}
+
+function getPathUrl(url) {
+    const urlObject = new URL(url);
+
+    const pathParts = urlObject.pathname.split('/').filter(part => part !== '');
+    const parentPath = pathParts.slice(0, -1).join('/') + '/';
+
+    if (url.endsWith('/')) {
+        return url;
+    }
+
+    return parentPath;
+}
+
+
+function setTranslate(translate) {
+    chrome.storage.local.set({'auto': (translate ? '1' : '0')});
+}
+
+function setPage(page, translate) {
+    if (!storage['page']) {
+        storage['page'] = {};
+    }
+    storage['page'][page] = translate;
+    chrome.storage.sync.set(storage);
+}
+
+function setPath(path, translate) {
+    if (!storage['path']) {
+        storage['path'] = {};
+    }
+    storage['path'][path] = translate;
+    chrome.storage.sync.set(storage);
+}
+
+function delAll() {
+    storage = {};
+    chrome.storage.sync.set(storage);
+}
+
+
+async function initialize() {
+    try {
+        const tabQueryPromise = new Promise((resolve, reject) => {
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                if (tabs && tabs[0]) {
+                    resolve(tabs[0].url);
+                }
+            });
         });
-    }
 
-    let setLanguage = function (language) {
-        $('[name=language]').each(function (index) {
-            if ($(this).data('lang') === language) {
-                $('div[name=language_name]').text($(this).find('img').attr('alt'));
-                $(this).addClass('active');
-            } else {
-                $(this).removeClass('active');
-            }
+        const storageGetPromise = new Promise((resolve, reject) => {
+            chrome.storage.sync.get().then((data) => {
+                resolve(data);
+            });
         });
+
+        const results = await Promise.all([tabQueryPromise, storageGetPromise]);
+
+        url = results[0];
+        storage = results[1];
+    } catch (error) {
+        console.error('초기화 오류 발생: ', error)
+    }
+}
+
+$(async function () {
+    await initialize();
+    const page = getPageUrl(url);
+    const path = getPathUrl(url);
+
+    if (!storage['page']) {
+        storage['page'] = {};
     }
 
-    let changeLanguage = function (language) {
-        setLanguage(language);
-        chrome.storage.sync.set({'language': language}).then(() => {
-            console.log('language is set ' + language);
-        });
+    if (!storage['path']) {
+        storage['path'] = {};
     }
 
-    let setAuto = function (auto) {
-        $('input[name=auto]').prop('checked', auto);
+    const isPage = storage['page'][page];
+    const isPath = storage['path'][path];
+    setTranslate(isPage || isPath);
+
+    if (isPage) {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').show();
+        $('.delPath').hide();
+        $('.delAll').show();
+    } else if (isPath) {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').hide();
+        $('.delPath').show();
+        $('.delAll').show();
+    } else {
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
     }
 
-    let changeAuto = function (auto_check) {
-        let auto = auto_check ? '1' : '0';
-        chrome.storage.sync.set({'auto': auto}).then(() => {
-            console.log('auto is set ' + auto);
-        });
-    }
-
-    init();
-
-    $('[name=language]').on('click', function () {
-        changeLanguage($(this).data('lang'));
+    $('[name=addPage]').on('click', function () {
+        setTranslate(true);
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').show();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPage(page, true);
     });
 
-    $('[name=auto]').on('click', function () {
-        changeAuto($(this).prop('checked'));
+    $('[name=addPath]').on('click', function () {
+        setTranslate(true);
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').hide();
+        $('.delPath').show();
+        $('.delAll').show();
+        setPath(path, true);
+    });
+
+    $('[name=delPage]').on('click', function () {
+        setTranslate(false);
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPage(page, false);
+    });
+
+    $('[name=delPath]').on('click', function () {
+        setTranslate(false);
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPath(path, false);
+    });
+
+    $('[name=delAll]').on('click', function () {
+        setTranslate(false);
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        delAll();
     });
 });
