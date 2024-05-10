afd9e8438899e6804499b09b83145ecaeb579e6a
mytechnic <mytechnic@gmail.com>
2024-05-10 16:05:36 +0900

    배포용 패키지 추가

---
+++ pack/StarPoeTranslate-v1.8/scripts/popup.js
@@ -0,0 +1,135 @@
+let isPageTranslate = false;
+let isPathTranslate = false;
+let path = '';
+let page = '';
+let cacheVersion = -1;
+let bookmark = {};
+
+function setPage(translate) {
+    if (!bookmark['page']) {
+        bookmark['page'] = {};
+    }
+
+    bookmark['page'][page] = translate;
+    setSyncStorage(bookmark);
+}
+
+function setPath(translate) {
+    if (!bookmark['path']) {
+        bookmark['path'] = {};
+    }
+    bookmark['path'][path] = translate;
+    setSyncStorage(bookmark);
+}
+
+function delAll() {
+    clearSyncStorage();
+}
+
+function changeEventListener() {
+    onChangeStorage((changes, namespace) => {
+        for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+            if (namespace === 'local' && key === 'version') {
+                $('#version').text(newValue);
+            }
+        }
+    });
+}
+
+async function initialize() {
+    let [tabs, localStorage, syncStorage] = await Promise.all([
+        tabsPromise(),
+        localStoragePromise(),
+        syncStoragePromise()
+    ]);
+
+    let currentUrl = '';
+    if (tabs && tabs[0]) {
+        currentUrl = tabs[0].url;
+    }
+    if (!localStorage || Object.keys(localStorage).length === 0) {
+        localStorage = {'version': -1, 'dictionary': {'h': {}, 'p': {}}, 'documentCache': {}};
+    }
+    if (!syncStorage || Object.keys(syncStorage).length === 0) {
+        syncStorage = {'page': {}, 'path': {}, 'autoTranslate': false};
+    }
+
+    bookmark = syncStorage;
+    page = getPageUrl(currentUrl);
+    path = getPathUrl(currentUrl);
+    isPageTranslate = syncStorage['page'][page] || false;
+    isPathTranslate = syncStorage['path'][path] || false;
+    cacheVersion = localStorage['version'];
+
+    $('#version').text(cacheVersion);
+}
+
+$(async function () {
+    await changeEventListener();
+    await initialize();
+
+    if (isPageTranslate) {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').show();
+        $('.delPath').hide();
+        $('.delAll').show();
+    } else if (isPathTranslate) {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').hide();
+        $('.delPath').show();
+        $('.delAll').show();
+    } else {
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+    }
+
+    $('button[name=addPage]').on('click', function () {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').show();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPage(true);
+    });
+
+    $('button[name=addPath]').on('click', function () {
+        $('.addPage').hide();
+        $('.addPath').hide();
+        $('.delPage').hide();
+        $('.delPath').show();
+        $('.delAll').show();
+        setPath(true);
+    });
+
+    $('button[name=delPage]').on('click', function () {
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPage(false);
+    });
+
+    $('button[name=delPath]').on('click', function () {
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        setPath(false);
+    });
+
+    $('button[name=delAll]').on('click', function () {
+        $('.addPage').show();
+        $('.addPath').show();
+        $('.delPage').hide();
+        $('.delPath').hide();
+        $('.delAll').show();
+        delAll();
+    });
+});
